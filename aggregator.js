eb999f4f6a4c10dafb3edd2003310fdf79a50bc7
Javier Berrocal <jberolm@unex.es>
2021-11-12 18:27:27 +0100

    Initial Commit
    Shopping Center Aggreegator

---
+++ aggregator.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.aggregate =function (result, method){
+
+
+    switch(method){
+        case 'getTemperature':
+            //TODO Operation or function to process the data
+            return result;
+        break;
+        case 'getMusic':
+            //TODO Operation or function to process the data
+            return result;
+        break;
+    }
+
+
+}
